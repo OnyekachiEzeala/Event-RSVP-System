b32569906ae8dddae8635613fc3ab370c81c3f1a
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2025-04-30 17:45:53 +0100

    ALT/SOE/024/2904, modified the post request and created the get event request

--- event_creation.py
+++ event_creation.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Optional
-
-from fastapi import FastAPI, File, UploadFile, Form
+from datetime import datetime
+from fastapi import FastAPI, File, UploadFile, Form, status, HTTPException
 from pydantic import BaseModel
 from uuid import UUID
 
@@ -8,35 +8,30 @@ app = FastAPI()
 
 
 class EventBase(BaseModel):
-  title: Annotated[str, Form()]
-  description: Annotated[str, Form()]
-  date: Annotated[str, Form()]
-  location: Annotated[str, Form()]
+  title: str
+  description: str
+  location: str
+  date : datetime = datetime.now().date()
   # flyer: Optional[Annotated[UploadFile, Form()]]
   # rsvp: list[Annotated[str, Form()]]
 
-class Event(EventBase):
-  id: int
+class CreateEvent(EventBase):
+  pass
 
-events: list[Event] = []
+events: list[CreateEvent] = []
 
-@app.post("/event/")
-async def event(
-  title: Annotated[str, Form()],
-  description: Annotated[str, Form()],
-  date: Annotated[str, Form()],
-  location: Annotated[str, Form()],
-):
-  event_id =str(UUID(int=len(events) + 1))
-  events.append(Event)
+# =======Post an Event=======
+@app.post("/events" , status_code=status.HTTP_201_CREATED)
+async def create_event(event : Annotated[EventBase, Form()]):
+  event_dict = event.model_dump()
+  event_dict['id'] =str(UUID(int=len(events) + 1))
+  event_dict['event_create_at'] = datetime.now()
+  events.append(event_dict)
+  return event_dict
 
-  
-  item_data = {
-    "id":  event_id,
-    "title": title,
-    "description": description,
-    "date": date,
-    "location": location
-  }
-  
-  return {"Message" : "Event created successfully!", "event_data" : item_data}
+# =========Listing the Events======
+@app.get("/events", status_code=status.HTTP_200_OK)
+async def list_event():
+  if not events:
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+  return events
