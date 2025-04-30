246e98c0419cb555643cce8d90310bc872646e7d
Kunlexzy0329 <solomonolakunle0329@gmail.com>
2025-04-30 20:34:03 +0100

    ALT/SOE/024/2904, modified date type

--- event.py
+++ event.py
@@ -1,5 +1,5 @@
 from typing import Annotated, Optional
-from datetime import datetime
+from datetime import datetime,date
 from fastapi import FastAPI, File, UploadFile, Form, status, HTTPException
 from pydantic import BaseModel
 from uuid import UUID
@@ -11,7 +11,7 @@ class EventBase(BaseModel):
   title: str
   description: str
   location: str
-  date : datetime = datetime.now().date()
+  date : date
   # flyer: Optional[Annotated[UploadFile, Form()]]
   # rsvp: list[Annotated[str, Form()]]
 
@@ -22,7 +22,7 @@ events: list[CreateEvent] = []
 
 # =======Post an Event=======
 @app.post("/events" , status_code=status.HTTP_201_CREATED)
-async def create_event(event : Annotated[EventBase, Form()]):
+async def create_event(event : Annotated[CreateEvent, Form()]):
   event_dict = event.model_dump()
   event_dict['id'] =str(UUID(int=len(events) + 1))
   event_dict['event_create_at'] = datetime.now()
