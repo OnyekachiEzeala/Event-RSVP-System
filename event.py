e82bf29751e037a1c44b14dfa64696c3b3ecda9a
Onyeka <ezesam227@gmail.com>
2025-05-01 23:18:37 +0100

    Removed obsolete files

--- event.py
+++ event.py
@@ -1,6 +1,6 @@
 from typing import Annotated, Optional
 from datetime import datetime,date
-from fastapi import FastAPI, File, UploadFile, Form, status, HTTPException
+from fastapi import FastAPI, File, UploadFile, Form, status, HTTPException, Depends
 from pydantic import BaseModel
 from uuid import UUID
 
@@ -12,19 +12,27 @@ class EventBase(BaseModel):
   description: str
   location: str
   date : date
-  # flyer: Optional[Annotated[UploadFile, Form()]]
-  # rsvp: list[Annotated[str, Form()]]
+  flyer: Optional[UploadFile] = None
+  rsvp: list [str] = []
 
 class Event(EventBase):
   pass
 
+class RSVP(BaseModel):
+  name: str
+  email: str
+
+
 events: list[EventBase] = []
+# rsvps: dict[str, list[RSVP]] = {}
+
+# store_email = []
 
 # =======Post an Event=======
 @app.post("/events" , status_code=status.HTTP_201_CREATED)
 async def create_event(event : Annotated[Event, Form()]):
   event_dict = event.model_dump()
-  event_dict['id'] = str(UUID(int=len(events) + 1))
+  event_dict['id'] = len(events) + 1 
   event_dict['event_create_at'] = datetime.now()
   events.append(event_dict)
   return event_dict
@@ -35,3 +43,24 @@ async def list_event():
   if not events:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
   return events
+
+# ========Get Events by ID========
+@app.get ("/events/{event_id}", status_code = status.HTTP_200_OK)
+def get_event_by_id(event_id:int):
+    for event in events:
+        if event["id"] == event_id:
+            return event
+    raise HTTPException(status_code=404, detail="Event not found")
+
+
+
+@app.post("/events/{event_id}/rsvp", status_code=status.HTTP_201_CREATED)
+async def rsvp_event(event_id:int, response: Annotated[RSVP, Form()]):
+  for event in events:
+    if event["id"] == event_id:
+        return {
+            "message": f"RSVP received for event {event_id}",
+            "rsvp": response,
+        }
+
+  raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Event not found")
